73e0e6e258f089aee85f31074990548bbe48dcfb
Rogério Rocha <rogerio.sf.rocha@gmail.com>
2025-05-28 18:10:13 +0100

    Hopefully OS solves it

--- src/main/java/KademliaDHT/Kademlia.java
+++ src/main/java/KademliaDHT/Kademlia.java
@@ -337,7 +337,7 @@ public class Kademlia {
                     }
                 });
 
-        bootstrap.localAddress(selfInfo.getPort());
+        //bootstrap.localAddress(selfInfo.getPort()+1);
         ChannelFuture future = bootstrap.connect(targetInfo.getIpAddr(), targetInfo.getPort()).sync();
         LOGGER.info("Connected to node " + targetInfo.getIpAddr() + ":" + targetInfo.getPort());
         future.channel().closeFuture().await(3, TimeUnit.SECONDS);
