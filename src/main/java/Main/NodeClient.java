c8fb892383aa5776eff15a14965c5792321e3146
Rogério Rocha <rogerio.sf.rocha@gmail.com>
2025-05-28 17:37:55 +0100

    Updated again

--- src/main/java/Main/NodeClient.java
+++ src/main/java/Main/NodeClient.java
@@ -43,7 +43,7 @@ public class NodeClient {
                 String bootstrapIp = bootstrapParts[0];
                 int bootstrapPort = Integer.parseInt(bootstrapParts[1]);
 
-                NodeInfo bootstrapInfo = kademliaNode.getNodeInfo();
+                NodeInfo bootstrapInfo = new NodeInfo(bootstrapIp, bootstrapPort);
                 kademliaNode.updateRoutingTable(bootstrapInfo);
                 dhtInstance.joinNetwork(kademliaNode, bootstrapInfo.getNodeId());
             }
