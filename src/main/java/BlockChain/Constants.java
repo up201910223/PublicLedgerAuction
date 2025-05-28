d623f84996ac7f32b6bc1e78e92ad8658ed643eb
Rogério Rocha <rogerio.sf.rocha@gmail.com>
2025-05-28 22:29:48 +0100

    Changed difficulty

--- src/main/java/BlockChain/Constants.java
+++ src/main/java/BlockChain/Constants.java
@@ -9,7 +9,7 @@ package main.java.BlockChain;
 public class Constants {
     private Constants(){}
 
-    public static final int DIFFICULTY = 3;
+    public static final int DIFFICULTY = 1;
 
     public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
 
